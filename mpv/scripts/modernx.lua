599e31ccde51fcc6139df922a71f223732f00883
Sharad Chandel <85397332+Sharad104@users.noreply.github.com>
2024-06-18 00:03:01 +0530

    modified modernx.lua's OSC Title font size and extended character limit
    Ex: Supernatural S02E20 What Is and What Should Never Be  [1080p x265 10bit Joy] 
    this title was not fully appearing in the modernx On Screen Controlls interface its was only half , i extended character limit a little and made title's font size smaller.

--- mpv/scripts/modernx.lua
+++ mpv/scripts/modernx.lua
@@ -155,7 +155,7 @@ local osc_styles = {
     Ctrl3 = '{\\blur0\\bord0\\1c&HFFFFFF&\\3c&HFFFFFF&\\fs24\\fnmaterial-design-iconic-font}',
     Time = '{\\blur0\\bord0\\1c&HFFFFFF&\\3c&H000000&\\fs17\\fn' .. user_opts.font .. '}',
     Tooltip = '{\\blur1\\bord0.5\\1c&HFFFFFF&\\3c&H000000&\\fs18\\fn' .. user_opts.font .. '}',
-    Title = '{\\blur1\\bord0.5\\1c&HFFFFFF&\\3c&H0\\fs38\\q2\\fn' .. user_opts.font .. '}',
+    Title = '{\\blur1\\bord0.5\\1c&HFFFFFF&\\3c&H0\\fs30\\q2\\fn' .. user_opts.font .. '}',
     WinCtrl = '{\\blur1\\bord0.5\\1c&HFFFFFF&\\3c&H0\\fs20\\fnmpv-osd-symbols}',
     elementDown = '{\\1c&H999999&}',
     elementHighlight = '{\\blur1\\bord1\\1c&HFFC033&}',
@@ -1346,7 +1346,7 @@ layouts = function ()
     lo.style = string.format('%s{\\clip(%f,%f,%f,%f)}', osc_styles.Title,
 								geo.x, geo.y - geo.h, geo.x + geo.w , geo.y + 5)
 	lo.alpha[3] = 0
-    lo.button.maxchars = geo.w / 23
+    lo.button.maxchars = geo.w / 16
 end
 
 -- Validate string type user options
